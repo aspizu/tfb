69fe5ac0d416a84c3deddc6540c8bab2ea011725
CJ Gilroy <cgilroy33@gmail.com>
2023-10-14 16:37:31 -0400

    added base for icons

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,51 @@
 use ansi_term::Color::*;
-use std::path::{Path, PathBuf};
+use std::{
+    ffi::OsStr,
+    path::{Path, PathBuf},
+};
 
 enum FileType {
     Directory,
-    Other,
+    File,
+    Symlink,
 }
 
-fn find_highlighting(path: PathBuf) -> String {
-    let name_with_prefix = path.display().to_string();
-    let name = name_with_prefix.strip_prefix("./").unwrap();
-    let file_type: FileType = if path.is_dir() {
-        FileType::Directory
-    } else {
-        FileType::Other
-    };
+impl FileType {
+    fn from_path(path: &PathBuf) -> FileType {
+        // proof of concept
 
+        if path.is_dir() {
+            return FileType::Directory;
+        } else if path.is_file() {
+            return FileType::File;
+        } else if path.is_symlink() {
+            return FileType::Symlink;
+        } else {
+            return FileType::File;
+        }
+    }
+}
+
+fn find_highlighting(file_type: &FileType, name: &str) -> String {
     match file_type {
         FileType::Directory => return RGB(66, 135, 245).paint(name).to_string(),
-        FileType::Other => return name.to_owned(),
+        FileType::File => return name.to_owned(),
+        FileType::Symlink => return RGB(176, 176, 62).paint(name).to_string(),
+    }
+}
+
+fn find_icon(path: &Path) -> &str {
+    if path.is_dir() {
+        return "󰉋";
+    };
+    let extension = path
+        .extension()
+        .unwrap_or(OsStr::new(""))
+        .to_str()
+        .unwrap_or("");
+    match extension {
+        "" => "󰈔",
+        &_ => "󰈔",
     }
 }
 
@@ -26,7 +54,15 @@ fn main() -> Result<(), anyhow::Error> {
     for entry in paths.read_dir()? {
         let path = entry?.path();
 
-        println!("{}", find_highlighting(path));
+        let icon = find_icon(&path);
+        let name = path.display().to_string();
+        let name = name.strip_prefix("./").unwrap();
+        let listing = format!("{} {}", icon, name);
+        let filetype = FileType::from_path(&path);
+
+        let listing_highlighted = find_highlighting(&filetype, &listing);
+
+        println!("{}", listing_highlighted);
     }
 
     Ok(())
